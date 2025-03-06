a66e8bb50a5f51e196c0076cc4fd6dd8c832d84d
Kyou12138 <1129057944@qq.com>
2025-03-06 20:35:17 +0800

    update config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,25 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    // Can be safely removed in newer versions of Next.js
+    future: {
+        // by default, if you customize webpack config, they switch back to version 4.
+        // Looks like backward compatibility approach.
+        webpack5: true,
+    },
+    webpack(config) {
+        config.resolve.fallback = {
+            // if you miss it, all the other options in fallback, specified
+            // by next.js will be dropped.
+            ...config.resolve.fallback,
+            stream: false,
+            net: false,
+            tls: false,
+            querystring: false,
+            crypto: false,
+        };
+        return config;
+    },
     /* config options here */
     images: {
         remotePatterns: [
